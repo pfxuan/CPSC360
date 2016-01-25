0a8d04a4758bfb441ccf62402deff2c510076913
pfxuan <pfxuan@gmail.com>
2016-01-25 12:12:00 -0500

    Update HW#1 testing script to match new requirements (Spring 2016)

--- hw1/runHW1Test.sh
+++ hw1/runHW1Test.sh
@@ -11,7 +11,7 @@ echo -e "Test 6: Documentation and coding style\n"
 
 ### Test 0: Compiling Test
 echo "*** Test 0: Compiling ***"
-tar xzf *.tgz &> /dev/null; tar xzf *.tar.gz &> /dev/null
+tar xzf *-hw1.tar.gz  &> /dev/null;
 make clean &> /dev/null; make depend &> /dev/null
 warningcount=`make 2>&1 | grep -i "warning" | wc -l`
 P0=0
@@ -20,11 +20,11 @@ if [ $warningcount -ge 1 ]; then
 fi
 
 if [ -e valueGuesser ] && [ -e valueServer ]; then
-  echo -e "GRADER: Test Result -> Passed.\n"
+  echo -e "DEBUGGER: Test Result -> Passed.\n"
   echo -e "===> Test 0 score (10 points max): 10.0 <===\n\n\n"
   P0=10
 else 
-  echo -e "GRADER: Test Result -> Failed!!!\n"
+  echo -e "DEBUGGER: Test Result -> Failed!!!\n"
   echo -e "(Cannot find the execution file ./valueGuesser or ./valueServer)\n"
   echo -e "===> Test 0 score (10 points max): 0.0 <===\n\n\n"
   P0=0
@@ -54,9 +54,9 @@ fi
 # 5) Check server log
 ##########################################
 echo "*** Test 1: Simple functionality test ***"
-exec 3< <(stdbuf -o0 ./valueServer 5001 123456)
+exec 3< <(stdbuf -o0 ./valueServer -p 5001 -v 123456)
 sleep 1
-./valueGuesser 127.0.0.1 5001 &> /dev/null & sleep 5; killall -q -9 valueGuesser &> /dev/null
+./valueGuesser -s 127.0.0.1 -p1 5001 -p2 9001 &> /dev/null & sleep 5; killall -q -9 valueGuesser &> /dev/null
 sleep 5
 killall -q -s SIGINT valueServer &> /dev/null; killall -q -9 valueServer &> /dev/null
 sleep 1
@@ -67,21 +67,21 @@ serverClientIPs=$(echo $serverLog | grep -E '127.0.0.1' | wc -l)
 
 P1=0;P10=0;P11=0;P12=0
 if [ $serverMsgRec -ge 1 ] && [ $serverClientIPs -ge 1 ]; then
-	echo -e "GRADER: get a correct result from server side (+50)."
+	echo -e "DEBUGGER: get a correct result from server side (+50)."
   P10=50
 else
-	echo -e "GRADER: can not correctly catch result from server side!"
+	echo -e "DEBUGGER: can not correctly catch result from server side!"
   if [ $serverMsgRec -ge 1 ]; then
-    echo "GRADER: server only can receive messages (+30)."
+    echo "DEBUGGER: server only can receive messages (+30)."
     P11=30
   else 
-    echo "GRADER: server can not recieve messages!"
+    echo "DEBUGGER: server can not recieve messages!"
   fi
   if [ $serverClientIPs -ge 1 ]; then
-    echo "GRADER: server only can get IP addresses from clients (+20)."
+    echo "DEBUGGER: server only can get IP addresses from clients (+20)."
     P12=20
   else 
-    echo "GRADER: server can not get IP addresses from clients!"
+    echo "DEBUGGER: server can not get IP addresses from clients!"
   fi
 fi
 P1=$[$P10+$P11+$P12]
@@ -103,9 +103,9 @@ sleep 2
 # 6) Check server log
 ##########################################
 echo "*** Test 2: Timeout test ***"
-./valueGuesser 127.0.0.1 5002 &> /dev/null &
+./valueGuesser -s 127.0.0.1 -p1 5002 -p2 9002 &> /dev/null &
 sleep 2
-exec 3< <(stdbuf -o0 ./valueServer 5002)
+exec 3< <(stdbuf -o0 ./valueServer -p 5002)
 sleep 2
 killall -q -s SIGINT valueServer &> /dev/null; killall -q -9 valueServer &> /dev/null
 killall -q -9 valueGuesser &> /dev/null
@@ -116,10 +116,10 @@ serverClientIPs=$(echo $serverLog | grep -E '127.0.0.1' | wc -l)
 
 P2=0;P20=0
 if [ $serverClientIPs -ge 1 ]; then
-	echo -e "GRADER: get a correct result from server side (+10)."
+	echo -e "DEBUGGER: get a correct result from server side (+10)."
   P20=10
 else
-	echo -e "GRADER: can not correctly catch result from server side!"
+	echo -e "DEBUGGER: can not correctly catch result from server side!"
 fi
 P2=$[$P20]
 echo -e "\n===> Test 2 score (10 points max): $P2 <===\n\n\n"
@@ -142,7 +142,7 @@ sleep 2
 echo "*** Test 3: CTRL-C test ***"
 exec 3< <(stdbuf -o0 ./valueServer 5003)
 sleep 1
-./valueGuesser 127.0.0.1 5003 &> /dev/null &
+./valueGuesser -s 127.0.0.1 -p1 5003 -p2 9003 &> /dev/null &
 sleep 5
 killall -q -s SIGINT valueServer &> /dev/null; sleep 1; killall -q -9 valueServer &> /dev/null
 sleep 1
@@ -155,10 +155,10 @@ serverClientIPs=$(echo $serverLog | grep -E '127.0.0.1' | wc -l)
 
 P3=0;P30=0;P31=0;P32=0
 if [ $serverMsgRec -ge 1 ] && [ $serverClientIPs -ge 1 ]; then
-	echo -e "GRADER: get a correct result from server side (+10)."
+	echo -e "DEBUGGER: get a correct result from server side (+10)."
   P30=10
 else
-	echo -e "GRADER: can not correctly catch result from server side!"
+	echo -e "DEBUGGER: can not correctly catch result from server side!"
 fi
 P3=$[$P30]
 echo -e "\n===> Test 3 score (10 points max): $P3 <===\n\n\n"
@@ -179,11 +179,11 @@ sleep 2
 # 6) Check server log
 ##########################################
 echo "*** Test 4: Multiple attempts test ***"
-exec 3< <(stdbuf -o0 ./valueServer 5004)
+exec 3< <(stdbuf -o0 ./valueServer -p 5004)
 sleep 1
-./valueGuesser 127.0.0.1 5004 &> /dev/null & sleep 10; killall -q -9 valueGuesser &> /dev/null
-./valueGuesser 127.0.0.1 5004 &> /dev/null & sleep 10; killall -q -9 valueGuesser &> /dev/null
-./valueGuesser 127.0.0.1 5004 &> /dev/null & sleep 10; killall -q -9 valueGuesser &> /dev/null
+./valueGuesser -s 127.0.0.1 -p1 5004 -p2 9004 &> /dev/null & sleep 10; killall -q -9 valueGuesser &> /dev/null
+./valueGuesser -s 127.0.0.1 -p1 5004 -p2 9004 &> /dev/null & sleep 10; killall -q -9 valueGuesser &> /dev/null
+./valueGuesser -s 127.0.0.1 -p1 5004 -p2 9004 &> /dev/null & sleep 10; killall -q -9 valueGuesser &> /dev/null
 sleep 1
 killall -q -s SIGINT valueServer &> /dev/null; killall -q -9 valueServer &> /dev/null
 sleep 1
@@ -194,10 +194,10 @@ serverClientIPs=$(echo $serverLog | grep -E '127.0.0.1.*,.*127.0.0.1.*,.*127.0.0
 
 P40=0
 if [ $serverMsgRec -ge 1 ] || [ $serverClientIPs -ge 1 ]; then
-	echo -e "GRADER: get a correct result from server side (+10)."
+	echo -e "DEBUGGER: get a correct result from server side (+10)."
   P40=10
 else
-	echo -e "GRADER: get an incorrect result from server side!"
+	echo -e "DEBUGGER: get an incorrect result from server side!"
 fi
 P4=$[$P40]
 echo -e "\n===> Test 4 score (10 points max): $P4 <===\n\n\n"
@@ -217,11 +217,11 @@ sleep 2
 ##########################################
 echo "*** Test 5: Multiple guessers test ***"
 hostIP=$(hostname -i)
-exec 3< <(stdbuf -o0 ./valueServer 5005)
+exec 3< <(stdbuf -o0 ./valueServer -p 5005)
 sleep 1
-./valueGuesser 127.0.0.1 5005 &> /dev/null & sleep 2; killall -q -9 valueGuesser &> /dev/null
-./valueGuesser $hostIP 5005 &> /dev/null & sleep 2; killall -q -9 valueGuesser &> /dev/null
-ssh imp22 "${PWD}/valueGuesser $hostIP 5005 &> /dev/null & sleep 2; killall -q -9 valueGuesser &> /dev/null"
+./valueGuesser -s 127.0.0.1 -p1 5005 -p2 9005 &> /dev/null & sleep 2; killall -q -9 valueGuesser &> /dev/null
+./valueGuesser -s $hostIP -p1 5005 -p2 9005 &> /dev/null & sleep 2; killall -q -9 valueGuesser &> /dev/null
+ssh imp22 "${PWD}/valueGuesser -s $hostIP -p1 5005 -p2 9005 &> /dev/null & sleep 2; killall -q -9 valueGuesser &> /dev/null"
 sleep 1
 killall -q -s SIGINT valueServer &> /dev/null; killall -q -9 valueServer &> /dev/null
 sleep 1
@@ -232,21 +232,21 @@ serverClientIPs=$(echo $serverLog | grep -oE "127.0.0.1.*,.*\b([0-9]{1,3}\.){3}[
 
 P5=0;P50=0;P51=0;P52=0
 if [ $serverMsgRec -ge 1 ] && [ $serverClientIPs -ge 1 ]; then
-	echo -e "GRADER: get a correct result from server side (+10)."
+	echo -e "DEBUGGER: get a correct result from server side (+10)."
   P50=10
 else
-	echo -e "GRADER: can not catch full results from server side!"
+	echo -e "DEBUGGER: can not catch full results from server side!"
   if [ $serverMsgRec -ge 1 ]; then
-    echo "GRADER: server only can receive messages (+5)."
+    echo "DEBUGGER: server only can receive messages (+5)."
     P51=5
   else 
-    echo "GRADER: server can not recieve messages!"
+    echo "DEBUGGER: server can not recieve messages!"
   fi
   if [ $serverClientIPs -ge 3 ]; then
-    echo "GRADER: server only can get IP addresses from clients (+5)."
+    echo "DEBUGGER: server only can get IP addresses from clients (+5)."
     P52=5
   else 
-    echo "GRADER: server can not get IP addresses from all clients!"
+    echo "DEBUGGER: server can not get IP addresses from all clients!"
   fi
 fi
 P5=$[$P50+$P51+$P52]
@@ -274,3 +274,4 @@ echo -e "\n===> Test 6 score (10 points max): $P6 <===\n"
 echo -e "\n\n\n----------------------------------"
 echo -e "Total Score (110 Points max): $[$P0+$P1+$P2+$P3+$P4+$P5+$P6]"
 echo -e "----------------------------------"
+echo -e "Note: the total score in this test script is only used to check incompatible implementation, and does not have a direct connection with your final HW#1 grade received from TA. We'll use different testing values and extra testing cases to further inspect your final submission."
